7b5e49913b6f1419c88ba0792f3c6223f372f898
linus123 <linus_pagan@hotmail.com>
2018-01-27 06:07:44 -0600

    System now builds database

--- build.ps1
+++ build.ps1
@@ -8,4 +8,32 @@ if (Test-Path $buildFolder) {
 
 mkdir $buildFolder | out-null
 
-&$nuGetExec Install psake -o "$buildFolder\package"
+&$nuGetExec Install psake -o "$buildFolder\packages"
+
+#----------------------
+
+remove-module [p]sake
+
+# find psake's path
+$psakeModule = (Get-ChildItem ("$buildFolder\packages\psake*\tools\psake\psake.psm1")).FullName | Sort-Object $_ | select -last 1
+ 
+Import-Module $psakeModule
+
+# you can write statements in multiple lines using `
+Invoke-psake -buildFile "$baseDir\src\Build\default.ps1"
+#			 -taskList Clean `
+#			 -framework 4.5.2 `
+#		     -properties @{ 
+#				 "buildConfiguration" = "Release"
+#				 "buildPlatform" = "Any CPU"} `
+#			 -parameters @{ 
+#				 "solutionFile" = "..\psake.sln"
+#				 "buildNumber" = $buildNumber
+#				 "branchName" = $branchName
+#				 "gitCommitHash" = $gitCommitHash
+#				 "isMainBranch" = $isMainBranch}
+
+Write-Host "Build exit code:" $LastExitCode
+
+# Propagating the exit code so that builds actually fail when there is a problem
+exit $LastExitCode
--- src/Build/default.ps1
+++ src/Build/default.ps1
@@ -1,16 +1,53 @@
 ﻿properties {
 	$projectName = "TinyReturns"
-	$baseDir = resolve-path .
+	$baseDir = resolve-path ..\..\
 
 	$buildFolder = "$baseDir\.build"
+	$packagesFolder = "$buildFolder\packages"
 	$srcFolder = "$baseDir\src"
+	$libFolder = "$baseDir\lib"
 	
 	$databaseServer = "(localdb)\MSSQLLocalDB"
 	$databaseName = $projectName
 
 	$databaseScriptsFolder = "$baseDir\data\mssql\TinyReturns"
-
-	$roundhouseExec = "$srcFolder\"
 	
 	$solutionFile = "$srcFolder\TinyReturns.sln"
+
+	$nuGetExec = (Find-PackagePath $libFolder "nuget") + "\nuget.exe"
+}
+
+task default -depends GetNuGetPackages, ResetDatabase
+
+FormatTaskName "`r`n`r`n-------- Executing {0} Task --------"
+
+task GetNuGetPackages {
+	Exec {
+		&$nuGetExec Install roundhouse -o $packagesFolder
+	}
+}
+
+task ResetDatabase {
+
+	$roundhouseExec = (Find-PackagePath $packagesFolder "roundhouse") + "\tools\rh.exe"
+
+	$versionFile = "$databaseScriptsFolder\_BuildInfo.xml"
+	$versionPath = "//buildInfo/version"
+
+	Exec {
+		&$roundhouseExec /s=$databaseServer /d=$databaseName /f=$databaseScriptsFolder /env=local /vf=$versionFile /vx=$versionPath /simple /silent
+	}
+}
+
+# --------------------------
+
+function Find-PackagePath
+{
+	[CmdletBinding()]
+	param(
+		[Parameter(Position=0,Mandatory=1)]$packagesPath,
+		[Parameter(Position=1,Mandatory=1)]$packageName
+	)
+
+	return (Get-ChildItem ($packagesPath + "\" + $packageName + "*")).FullName | Sort-Object $_ | select -last 1
 }
