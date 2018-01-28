88e7e0e48451c6674045523c5a34bb80bc4b4755
linus123 <linus_pagan@hotmail.com>
2018-01-28 04:07:43 -0600

    Added file IO

---
+++ src/FileIo/CitiReturnsFileReader.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Linq;
+using CsvHelper;
+using Dimensional.TinyReturns.Core;
+using Dimensional.TinyReturns.Core.CitiFileImport;
+
+namespace TinyReturns.FileIo
+{
+    public class CitiReturnsFileReader : ICitiReturnsFileReader
+    {
+        private readonly ISystemLog _systemLog;
+
+        public CitiReturnsFileReader(
+            ISystemLog systemLog)
+        {
+            _systemLog = systemLog;
+        }
+
+        public CitiMonthlyReturnsDataFileRecord[] ReadFile(
+            string filePath)
+        {
+            var streamReader = new StreamReader(filePath);
+
+            var csv = new CsvReader(streamReader);
+
+            var records = csv.GetRecords<CitiMonthlyReturnsDataFileRecord>();
+
+            return records.ToArray();
+        }
+    }
+}
