a0f104314e87cf61017d76b3ede93f871c65610e
linus123 <linus_pagan@hotmail.com>
2018-04-22 14:30:30 -0500

    Added first test

---
+++ src/Core/MutualFundManagement/MutualFundRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace TinyReturns.Core.MutualFundManagement
+{
+    public class MutualFundRepository
+    {
+        public IMaybe<MutualFund> GetByTickerSymbol(
+            string tickerSymbol)
+        {
+            return new MaybeNoValue<MutualFund>();
+        }
+    }
+}
--- src/IntegrationTests/Core/MutualFundManagementTests.cs
+++ src/IntegrationTests/Core/MutualFundManagementTests.cs
@@ -1,16 +1,26 @@
-﻿using TinyReturns.Core.MutualFundManagement;
+﻿using TinyReturns.Core;
+using TinyReturns.Core.MutualFundManagement;
 using Xunit;
 
 namespace TinyReturns.IntegrationTests.Core
 {
     public class MutualFundManagementTests
     {
-        [Fact]
-        public void ShouldWork()
+        [Fact(DisplayName = "Repository should not find a mutual fund when ticker symbol not found.")]
+        public void Test0010()
         {
             var serviceLocator = new ServiceLocatorForIntegrationTests();
 
-            serviceLocator.GetService<IMutualFundEvenDataTableGateway>();
+            var mutualFundEvenDataTableGateway = serviceLocator.GetService<IMutualFundEvenDataTableGateway>();
+
+            mutualFundEvenDataTableGateway.DeleteAll();
+
+            var mutualFundRepository = new MutualFundRepository();
+
+            var mutualFundResult = mutualFundRepository.GetByTickerSymbol("ABC");
+
+            Assert.False(mutualFundResult.HasValue);
+            Assert.True(mutualFundResult.HasNoValue());
         }
 
     }
