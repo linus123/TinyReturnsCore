c91a4134c1df361fe5eb997cb4878af737044459
linus123 <linus_pagan@hotmail.com>
2018-05-18 05:59:48 -0500

    Refactored tests builder

--- src/UnitTests/Core/InvestmentVehicleFactoryForTests.cs
+++ src/UnitTests/Core/InvestmentVehicleFactoryForTests.cs
@@ -6,68 +6,65 @@ namespace TinyReturns.UnitTests.Core
 {
     public class InvestmentVehicleFactoryForTests
     {
-        public static InvestmentVehicleSetupForTests SetupPortfolio(
+        public static InvestmentVehicleFactoryForTests SetupPortfolio(
             int number, string name)
         {
             var portfolio = InvestmentVehicle.CreatePortfolio(number, name);
 
-            return new InvestmentVehicleSetupForTests(portfolio);
+            return new InvestmentVehicleFactoryForTests(portfolio);
         }
 
-        public static InvestmentVehicleSetupForTests SetupBenchmark(
+        public static InvestmentVehicleFactoryForTests SetupBenchmark(
             int number, string name)
         {
             var portfolio = InvestmentVehicle.CreateBenchmark(number, name);
 
-            return new InvestmentVehicleSetupForTests(portfolio);
+            return new InvestmentVehicleFactoryForTests(portfolio);
         }
 
-        public class InvestmentVehicleSetupForTests
+        private readonly InvestmentVehicle _investmentVehicle;
+
+        public InvestmentVehicleFactoryForTests(
+            InvestmentVehicle investmentVehicle)
         {
-            private readonly InvestmentVehicle _investmentVehicle;
+            _investmentVehicle = investmentVehicle;
+        }
 
-            public InvestmentVehicleSetupForTests(
-                InvestmentVehicle investmentVehicle)
+        public InvestmentVehicleFactoryForTests AddNetReturn(
+            MonthYear monthYear,
+            decimal returnValue)
+        {
+            return AddReturn(monthYear, returnValue, FeeType.NetOfFees);
+        }
+
+        public InvestmentVehicleFactoryForTests AddGrossReturn(
+            MonthYear monthYear,
+            decimal returnValue)
+        {
+            return AddReturn(monthYear, returnValue, FeeType.GrossOfFees);
+        }
+
+        private InvestmentVehicleFactoryForTests AddReturn(
+            MonthYear monthYear,
+            decimal returnValue,
+            FeeType feeType)
+        {
+            var netSeries = _investmentVehicle.GetAllReturnSeries().FirstOrDefault(s => { return s.FeeType == feeType; });
+
+            if (netSeries == null)
             {
-                _investmentVehicle = investmentVehicle;
+                netSeries = new MonthlyReturnSeries() {FeeType = feeType};
+                _investmentVehicle.AddReturnSeries(netSeries);
             }
 
-            public InvestmentVehicleSetupForTests AddNetReturn(
-                MonthYear monthYear,
-                decimal returnValue)
-            {
-                return AddReturn(monthYear, returnValue, FeeType.NetOfFees);
-            }
+            netSeries.AddReturn(monthYear, returnValue);
 
-            public InvestmentVehicleSetupForTests AddGrossReturn(
-                MonthYear monthYear,
-                decimal returnValue)
-            {
-                return AddReturn(monthYear, returnValue, FeeType.GrossOfFees);
-            }
+            return this;
+        }
 
-            private InvestmentVehicleSetupForTests AddReturn(
-                MonthYear monthYear,
-                decimal returnValue,
-                FeeType feeType)
-            {
-                var netSeries = _investmentVehicle.GetAllReturnSeries().FirstOrDefault(s => { return s.FeeType == feeType; });
-
-                if (netSeries == null)
-                {
-                    netSeries = new MonthlyReturnSeries() {FeeType = feeType};
-                    _investmentVehicle.AddReturnSeries(netSeries);
-                }
-
-                netSeries.AddReturn(monthYear, returnValue);
-
-                return this;
-            }
-
-            public InvestmentVehicle Create()
-            {
-                return _investmentVehicle;
-            }
+        public InvestmentVehicle Create()
+        {
+            return _investmentVehicle;
         }
     }
 }
